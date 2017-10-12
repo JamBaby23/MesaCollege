a2d75cb2c3c0bf595f01b10cf4f7643d501d12f5
Jammal <mr.loiseau23@gmail.com>
2017-10-11 22:51:57 -0700

    final touches

--- app/src/main/java/edu/sdmesa/mesacollege/MainActivity.java
+++ app/src/main/java/edu/sdmesa/mesacollege/MainActivity.java
@@ -2,23 +2,53 @@ package edu.sdmesa.mesacollege;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 
-public class MainActivity extends AppCompatActivity
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
 
-    WebView mesaView;
+    Button buttonWebsite;
+    Button buttonCall;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_web);
 
-        mesaView = (WebView) findViewById(R.id.mesaWebView);
-        mesaView.setWebViewClient(new WebViewClient());
-        mesaView.loadUrl("*http://www.sdmesa.edu/*");
+        buttonWebsite = (Button) findViewById(R.id.buttonWebsite);
+        buttonWebsite.setOnClickListener(this);
 
+        buttonCall = (Button) findViewById(R.id.buttonCall);
+        buttonCall.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View arg0)
+            {
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:6193882600"));
+
+                if (ActivityCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED)
+                {
+                    return;
+                }
+
+                startActivity(callIntent);
+            }
+
+        });
+    }
+    @Override
+    public void onClick(View v)
+    {
+        Intent m = new Intent(this,WebActivity.class);
+        startActivity(m);
     }
 }
--- app/src/main/java/edu/sdmesa/mesacollege/WebActivity.java
+++ app/src/main/java/edu/sdmesa/mesacollege/WebActivity.java
@@ -2,52 +2,23 @@ package edu.sdmesa.mesacollege;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.content.Intent;
-import android.view.View;
-import android.widget.Button;
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.support.v4.app.ActivityCompat;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
-
-public class WebActivity extends AppCompatActivity implements View.OnClickListener
+public class WebActivity extends AppCompatActivity
 {
 
-    Button buttonWebsite;
-    Button buttonCall;
+    WebView mesaView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_web);
+        setContentView(R.layout.activity_main);
 
-        buttonWebsite = (Button) findViewById(R.id.buttonWebsite);
-        buttonWebsite.setOnClickListener(this);
+        mesaView = (WebView) findViewById(R.id.mesaWebView);
+        mesaView.setWebViewClient(new WebViewClient());
+        mesaView.loadUrl("*http://www.sdmesa.edu/*");
 
-        buttonCall = (Button) findViewById(R.id.buttonCall);
-        buttonCall.setOnClickListener(new View.OnClickListener()
-        {
-            public void onClick(View arg0)
-            {
-                Intent callIntent = new Intent(Intent.ACTION_CALL);
-                callIntent.setData(Uri.parse("tel:6193882600"));
-
-                if (ActivityCompat.checkSelfPermission(WebActivity.this,
-                        Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED)
-                {
-                    return;
-                }
-
-                startActivity(callIntent);
-            }
-
-            });
-    }
-    @Override
-    public void onClick(View v)
-    {
-        Intent m = new Intent();
     }
 }
