033bc11f5bcd35e6eab5bf7b7ff6495c2ab83f72
Jammal <mr.loiseau23@gmail.com>
2017-10-12 11:00:12 -0700

    final touches

--- app/src/main/java/edu/sdmesa/mesacollege/MainActivity.java
+++ app/src/main/java/edu/sdmesa/mesacollege/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     return;
                 }
 
-                startActivity(callIntent);
+                //startActivity(callIntent);
             }
 
         });
